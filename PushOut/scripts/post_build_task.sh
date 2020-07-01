cc96e3dfc40beb5d225c842b33fc84bd97dc9c5c
kimi <kimi10.develop@gmail.com>
2020-07-01 23:22:36 +0900

    add paswd

--- PushOut/scripts/post_build_task.sh
+++ PushOut/scripts/post_build_task.sh
@@ -5,7 +5,7 @@ echo "Uploading IPA to Appstore Connect..."
 #Path is "$WORKSPACE/.build/last/<BUILD_TARGET_ID>/build.ipa"
 path="$WORKSPACE/.build/last/qa_ios/build.ipa"
  
-if xcrun altool --upload-app -f $path -u kimi10.develop@gmail.com -p $password ; then
+if xcrun altool --upload-app -f $path -u kimi10.develop@gmail.com -p lafz-hkhx-erdx-mbqu ; then
     echo "Upload IPA to Appstore Connect finished with success"
 else
     echo "Upload IPA to Appstore Connect failed"
